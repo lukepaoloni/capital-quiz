9dfb086e8b1c03812b4988aae5cb0d087ec08616
Luke Paoloni <lukepaoloni.me@gmail.com>
2025-02-28 09:10:06 +0000

    Refactor CountryQuestionGenerator to improve capital selection logic

--- app/Domain/Quiz/CountryQuestionGenerator.php
+++ app/Domain/Quiz/CountryQuestionGenerator.php
@@ -5,7 +5,6 @@ namespace App\Domain\Quiz;
 use App\Domain\Country\Contracts\CountryProviderInterface;
 use App\Domain\Country\Exceptions\CountryProviderException;
 use App\Domain\Quiz\DataObjects\Question;
-use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Session;
@@ -26,28 +25,31 @@ class CountryQuestionGenerator
     {
         $shownCapitals = Session::get('capitalsShown', []);
         /** @var Collection $capitals */
-        $capitals = Cache::rememberForever('capitals', function () {
-            return $this->countryProvider->getAll();
-        });
+        $capitals = Cache::rememberForever('capitals', fn() => $this->countryProvider->getAll());
 
         if (count($shownCapitals) === $capitals->count()) {
-            Session::remove('capitalsShown');
+            Session::forget('capitalsShown');
             $shownCapitals = [];
         }
 
-        $randomCapital = null;
-        do {
-            $randomCapital = Arr::random($capitals->toArray());
-            if (in_array($randomCapital['capital'], $shownCapitals)) {
-                $randomCapital = null;
-            }
-        } while(!$randomCapital);
+        $availableCapitals = $capitals->filter(fn($capital) => !in_array($capital['capital'], $shownCapitals));
 
-        $capitalsWithoutRandomCapital = $capitals->filter(fn ($capital) => $capital['iso2'] !== $randomCapital['iso2']);
-        $otherAnswers = collect(Arr::random($capitalsWithoutRandomCapital->toArray(), 2))->pluck('capital');
+        if ($availableCapitals->isEmpty()) {
+            throw new CountryProviderException('No available capitals to generate a question.');
+        }
+
+        // Add the selected capital to the list of shown capitals
+        $randomCapital = $availableCapitals->random();
+
+        $shownCapitals[] = $randomCapital['capital'];
+        Session::put('capitalsShown', $shownCapitals);
+
+        $capitalsWithoutRandomCapital = $capitals->reject(fn($capital) => $capital['iso2'] === $randomCapital['iso2']);
+        $otherAnswers = $capitalsWithoutRandomCapital->random(2)->pluck('capital');
 
         $question = __('quiz.question', ['country' => $randomCapital['name']]);
-        $allAnswers = [$randomCapital['capital'], ...$otherAnswers];
+        $allAnswers = [$randomCapital['capital'], ...$otherAnswers->all()];
+
         $this->_randomiseCorrectAnswerPosition($allAnswers, 0);
 
         return new Question(
